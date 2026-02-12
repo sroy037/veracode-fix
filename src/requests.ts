fb8cf8e77cd1b0e7432f59738743259b1d639c10
Soumik Roy <soroy@veracode.com>
2026-02-12 12:46:20 +0530

    removed extract debug lines

--- src/requests.ts
+++ src/requests.ts
@@ -337,8 +337,7 @@ async function makeRequestBatch(credentials:any, projectId:any, options:any) {
             console.log(response.data);
             console.log('#######- DEBUG MODE -#######')
         }
-        console.log(response.data)
-        console.log('hasMore value is: '+response.data.hasMore)
+
         if ( response.data.hasMore == true){
             console.log('More fixes are being generated. Retrying in 10 seconds.');
 
